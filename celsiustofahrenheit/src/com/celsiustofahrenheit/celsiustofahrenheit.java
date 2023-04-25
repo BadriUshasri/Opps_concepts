385a99e0c81490ca3ab632bd7466444cbb14cd05
BADRI USHASRI <BADRI USHASRI@DESKTOP-42JJVC9>
2023-04-25 12:41:41 +0530

    Converting c to f

---
+++ celsiustofahrenheit/src/com/celsiustofahrenheit/celsiustofahrenheit.java
@@ -0,0 +1,24 @@
+package com.celsiustofahrenheit;
+
+public class celsiustofahrenheit {
+public static void main(String[] args) {
+	       
+	        double celsius = 20.0, fahrenheit = 0.0;
+	        
+	      
+	        fahrenheit = (celsius * 1.8) + 32;
+	        System.out.println(
+	            " value of temperature in fahrenheit:"
+	            + fahrenheit);
+
+	        double fahrenheit1 = 80.0, celsius1 = 0.0;
+	         celsius1 =(fahrenheit1 - 32)*5/9;
+	         System.out.println("value of temperature in celsius:" + celsius1);
+				// TODO Auto-generated method stub
+
+			}
+	        
+	    }
+	
+
+
