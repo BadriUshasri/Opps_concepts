723913527c7ba8df2cc09051b732906e1a326712
BADRI USHASRI <BADRI USHASRI@DESKTOP-42JJVC9>
2023-04-25 13:17:24 +0530

    GroupofPersons

---
+++ GroupOfPersons/src/com/groupofpersons/GroupOfPersons.java
@@ -0,0 +1,18 @@
+package com.groupofpersons;
+
+public class GroupOfPersons {
+ public static void main(String[] args) {
+	String[] Persons = new String[7];
+	Persons[0]= "Teena";
+	Persons[1]= "Meena";
+	Persons[2]= "Reena"; 
+    Persons[3]= "Veena";
+	Persons[4]=	"Heena";
+	Persons[5]= "Seena";
+    Persons[6]= "Jeena";
+//    System.out.println(Persons[1]);
+    for (int i = 0; i<Persons.length;i++) {
+    	System.out.println(Persons[i]);
+    }
+}
+}
