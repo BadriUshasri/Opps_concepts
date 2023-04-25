c340dfd024de00396a637bc7ed9d13400c50c78b
BADRI USHASRI <BADRI USHASRI@DESKTOP-42JJVC9>
2023-04-25 13:24:30 +0530

    Min Elements

---
+++ Minelements/src/com/minelements/MinimumElements.java
@@ -0,0 +1,16 @@
+package com.minelements;
+
+public class MinimumElements {
+public static void main(String[] args) {
+	int [] arr = new int[] {30,87,4,26,8,5,91};
+	int min = arr[0];
+	for(int i =0;i<arr.length;i++) {
+		int i= 0;
+		if(arr[i]<min);
+		min = arr[i];
+		System.out.println("min elements in an given array:" +min);
+		
+	}
+	
+}
+}
