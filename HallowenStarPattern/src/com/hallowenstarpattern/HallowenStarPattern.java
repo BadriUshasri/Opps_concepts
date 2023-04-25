d77730e4358511074329aa420cd26ee04b12f382
BADRI USHASRI <BADRI USHASRI@DESKTOP-42JJVC9>
2023-04-25 13:18:19 +0530

    Star Pattern

---
+++ HallowenStarPattern/src/com/hallowenstarpattern/HallowenStarPattern.java
@@ -0,0 +1,32 @@
+package com.hallowenstarpattern;
+
+  public class HallowenStarPattern {
+    public static void main(String[] args) {
+    	
+	int size=6;
+	
+	for(int i=0;i<size;i++) {
+		
+	for(int j =0;j<size;j++) {
+		
+		if(i==0||i==size-1) {
+		System.out.print("*");
+		}
+		else {
+			if(j==0||j==size-1) {
+			System.out.print("*");
+		}
+	
+		else 
+		{
+			System.out.print(" ");
+			}
+		}
+	
+			}
+	System.out.println();
+	
+}
+}
+}
+
